cbed9693e5a8c6a35f249119f05b38ca03f31ff8
Bruno Quenan <82984295+BrunoQuenan98@users.noreply.github.com>
2024-08-29 19:59:07 +0000

    test

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,12 @@
 // Placeholder to host a component
+export const Hours = () => {
+    const day = new Date().getDay()
+
+    if(day === 0 || day === 6){
+        return <>9 a.m. a 8 p.m</>
+    }
+
+    return <>
+        10 a.m. a 4 p.m
+    </>
+}
